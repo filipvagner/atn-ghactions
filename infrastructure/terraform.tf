59b2f40090e7312712c5a803416b81269dd76df8
Filip Vagner <filip.vagner@orbit.cz>
2025-01-07 13:27:29 +0100

    Fixed backend

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -1,11 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
   }
-  
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
